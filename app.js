d7177a702c0384b02d8bc16e0e396d4e05419123
cw chen <sky85245630@gmail.com>
2021-03-08 22:38:34 +0800

    update

--- app.js
+++ app.js
@@ -6,9 +6,11 @@ var bodyParser = require('body-parser');
 var admin = require("firebase-admin");
 var serviceAccount = require("./nodejsqq-firebase-adminsdk-zieci-fa7aefceb6.json");
 admin.initializeApp({
-credential: admin.credential.cert(serviceAccount),
-databaseURL: "https://nodejsqq.firebaseio.com"
+   credential: admin.credential.cert(serviceAccount),
+   databaseURL: "https://nodejsqq.firebaseio.com"
 });
+
+
 
 var fireData = admin.database();
 
@@ -18,30 +20,34 @@ var fireData = admin.database();
 // })
 
 //把資料傳入firebase asd裡面
-fireData.ref('asd').set({"keyQQ":"valueQQ"}).then(
-   ()=>{
-      fireData.ref('asd').once('value',(e)=>{
-         console.log('已寫入',e.val())
-         })
+fireData.ref('asd').set({
+   "keyQQ": "valueQQ"
+}).then(
+   () => {
+      fireData.ref('asd').once('value', (e) => {
+         console.log('已寫入', e.val())
+      })
    }
-   
+
 )
 
 
-app.engine('ejs',engine);
-app.set('views','./views');
-app.set('view engine','ejs');
+app.engine('ejs', engine);
+app.set('views', './views');
+app.set('view engine', 'ejs');
 //增加靜態檔案的路徑
 app.use(express.static('public'))
 
 // 增加 body 解析
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.urlencoded({
+   extended: false
+}))
 
 //路由
-app.get('/',function(req,res){
+app.get('/', function (req, res) {
    res.render('index');
-    
+
 })
 
 // 監聽 port
